88fdfa7c705d73aefc274ddf74b37b4d3e73534e
kasskata <kassskata@gmail.com>
2026-01-28 17:50:58 +0200

    Add brackets

--- 01.C#Introduction/08.ConditionalStatementsAdvanced-Exercise/01. Cinema/Program.cs
+++ 01.C#Introduction/08.ConditionalStatementsAdvanced-Exercise/01. Cinema/Program.cs
@@ -9,20 +9,30 @@ Normal
 
 Discount
 12
-30 
+30
 */
+
 class Program
 {
     static void Main()
     {
-        string type = Console.ReadLine(); 
-        int rows = int.Parse(Console.ReadLine()); 
-        int cols = int.Parse(Console.ReadLine());  
+        string type = Console.ReadLine();
+        int rows = int.Parse(Console.ReadLine());
+        int cols = int.Parse(Console.ReadLine());
 
         double price = 0;
-        if (type == "Premiere") price = 12.0;
-        else if (type == "Normal") price = 7.5;
-        else if (type == "Discount") price = 5.0;
+        if (type == "Premiere")
+        {
+            price = 12.0;
+        }
+        else if (type == "Normal")
+        {
+            price = 7.5;
+        }
+        else if (type == "Discount")
+        {
+            price = 5.0;
+        }
 
         double total = rows * cols * price;
 
