d138a787104271e9dc3d850f2995ad6b8e6a4835
kasskata <kassskata@gmail.com>
2026-03-25 09:53:54 +0200

    Add excercise Methods List and TextProcessing

---
+++ 02.C#Fundamentals/10.Excercise.Lists/02.ChangeList/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+1 2 3 4 5 5 5 6
+Delete 5
+Insert 10 1
+Delete 5
+end
+
+20 12 4 319 21 31234 2 41 23 4
+Insert 50 2
+Insert 50 5
+Delete 4
+end
+*/
+
+internal class Program
+{
+    static void Main()
+    {
+        List<int> list = Console.ReadLine()
+            .Split()
+            .Select(e => int.Parse(e))
+            .OrderByDescending(e => e)
+            .ToList();
+
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] arguments = input.Split();
+            int element;
+
+            switch (arguments[0])
+            {
+                case "Insert":
+                {
+                    element = int.Parse(arguments[1]);
+                    int position = int.Parse(arguments[2]);
+                    list.Insert(position, element);
+                    break;
+                }
+                case "Delete":
+                    element = int.Parse(arguments[1]);
+                    for (int i = list.Count - 1; i >= 0; i--) 
+                    {
+                        if (list[i] == element)
+                        {
+                            list.RemoveAt(i);
+                        }
+                    }
+
+                    break;
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", list));
+    }
+}
