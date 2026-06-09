d80dcb032e654d6e2fadc028807544bac91f363e
kasskata <kassskata@gmail.com>
2026-06-09 09:40:08 +0300

    Add 14.Exercise-ExceptionHandlingAndReflection

---
+++ 03.C#OOP/14.Exercise-ExceptionHandlingAndReflection/02.PlayCatch/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace PlayCatch
+{
+public class Program
+{
+    public static void Main()
+    {
+        int[] array = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        int exceptionsCount = 0;
+
+        while (exceptionsCount < 3)
+        {
+            string[] commandArgs = Console.ReadLine().Split();
+            string action = commandArgs[0];
+
+            try
+            {
+                if (action == "Replace")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    int element = int.Parse(commandArgs[2]);
+                    array[index] = element;
+                }
+                else if (action == "Print")
+                {
+                    int startIndex = int.Parse(commandArgs[1]);
+                    int endIndex = int.Parse(commandArgs[2]);
+
+                    List<int> result = new List<int>();
+                    for (int i = startIndex; i <= endIndex; i++)
+                    {
+                        result.Add(array[i]);
+                    }
+
+                    Console.WriteLine(string.Join(", ", result));
+                }
+                else if (action == "Show")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    Console.WriteLine(array[index]);
+                }
+            }
+            catch (Exception exception)
+            {
+                exceptionsCount++;
+                Type exceptionType = exception.GetType();
+
+                if (exceptionType == typeof(FormatException))
+                {
+                    Console.WriteLine("The variable is not in the correct format!");
+                }
+                else if (exceptionType == typeof(IndexOutOfRangeException))
+                {
+                    Console.WriteLine("The index does not exist!");
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", array));
+    }
+}
+}
---
+++ 03.C#OOP/14.Exercise-ExceptionHandlingAndReflection/03.MoneyTransactions/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace MoneyTransactions
+{
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<int, BankAccount> accounts = new Dictionary<int, BankAccount>();
+
+        string[] input = Console.ReadLine().Split(',');
+
+        foreach (string acc in input)
+        {
+            string[] data = acc.Split('-');
+            int id = int.Parse(data[0]);
+            double balance = double.Parse(data[1]);
+
+            accounts.Add(id, new BankAccount(id, balance));
+        }
+
+        string command;
+        while ((command = Console.ReadLine()) != "End")
+        {
+            string[] arguments = command.Split();
+            string action = arguments[0];
+
+            try
+            {
+                if (action != "Deposit" && action != "Withdraw")
+                {
+                    throw new ArgumentException();
+                }
+
+                int accountId = int.Parse(arguments[1]);
+                double sum = double.Parse(arguments[2]);
+
+                if (!accounts.ContainsKey(accountId))
+                {
+                    throw new KeyNotFoundException();
+                }
+
+                BankAccount currentAccount = accounts[accountId];
+
+                if (action == "Deposit")
+                {
+                    currentAccount.Deposit(sum);
+                }
+                else if (action == "Withdraw")
+                {
+                    currentAccount.Withdraw(sum);
+                }
+
+                Console.WriteLine($"Account {accountId} has new balance: {currentAccount.Balance:F2}");
+            }
+            catch (Exception ex)
+            {
+                Type exceptionType = ex.GetType();
+
+                if (exceptionType == typeof(ArgumentException))
+                {
+                    Console.WriteLine("Invalid command!");
+                }
+                else if (exceptionType == typeof(KeyNotFoundException))
+                {
+                    Console.WriteLine("Invalid account!");
+                }
+                else if (exceptionType == typeof(InvalidOperationException))
+                {
+                    Console.WriteLine("Insufficient balance!");
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Enter another command");
+            }
+        }
+    }
+}
+
+public class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public double Balance { get; set; }
+
+    public BankAccount(int accountNumber, double balance)
+    {
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
+
+    public void Deposit(double sum)
+    {
+        Balance += sum;
+    }
+
+    public void Withdraw(double sum)
+    {
+        if (Balance < sum)
+        {
+            throw new InvalidOperationException();
+        }
+
+        Balance -= sum;
+    }
+}
+}
